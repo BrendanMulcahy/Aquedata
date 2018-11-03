6fdc46a890180a5c241f9fa4e20e9cae50a67d74
Brendan Mulcahy <mulcahy.software@gmail.com>
2018-11-03 18:02:40 -0400

    Add swashbuckle

--- Aquedata/Startup.cs
+++ Aquedata/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Aquedata
 {
@@ -20,12 +21,18 @@ namespace Aquedata
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
             services.AddHangfire(x =>
                 x.UseSqlServerStorage(@"Server=.; Database=Hangfire; Integrated Security=SSPI;",
                     new SqlServerStorageOptions
                     {
                         PrepareSchemaIfNecessary = true
                     }));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Aquedata", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -44,6 +51,13 @@ namespace Aquedata
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
